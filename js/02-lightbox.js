74422a46af7ce2f2222372c08df46ac36c68f55e
xottabbu4 <113995327+xottabbu4@users.noreply.github.com>
2022-11-18 22:26:52 +0200

    completed hw

---
+++ js/02-lightbox.js
@@ -0,0 +1,28 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const cardMarkup = createGalleryMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ original, preview, description }) => {
+      return `
+			<li>
+				<a class="gallery__item"
+					href="${original}">
+	    			<img
+	      			class="gallery__image"
+	      			src="${preview}"
+	      			alt="${description}"
+	    			/>
+	  		</a>
+			</li>`;
+    })
+    .join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
